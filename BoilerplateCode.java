cbfe3f4042ff732712ef1d667c09810c48135848
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-04-30 13:05:40 +0530

    variables in java & add terms in boilerplate

--- BoilerplateCode.java
+++ BoilerplateCode.java
@@ -1,6 +1,10 @@
 // Lecture 1 - Boilerplate code 
 public class BoilerplateCode {
-    // main content or code which excecute
-    
+    public static void main(String[] args) {
+        // main content or code which excecute   
+    }
 }
-// boilerplate code
+
+// Basic Terms 
+// ; -> Terminator, which is must to end the statement
+//  -> This is comment, which is not be included as code.
---
+++ Variables.java
@@ -0,0 +1,17 @@
+// Lecture 3 - Variables in java
+public class Variables {
+    public static void main(String[] args) {
+        //  A variable is a name given to a memory location in a program. It is used to store data that can be changed during the execution of the program. Variables are used to hold values that can be referenced and manipulated in a program. In Java, variables are created when a value is assigned to them using the assignment operator `=`. 
+        
+        // Example 
+
+        int a = 10;        // int -> datatype, a -> variable, 10 -> value/data 
+        int b = 5;         // b -> variable, 5 -> value/data
+
+        System.out.println(a);
+        System.out.println(b);
+
+        String name = "Piyush bhatt";
+        System.out.println(name);
+    }
+}
