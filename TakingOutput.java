0a5a45b16d753258b9b92d8762c1a2a18d127ccb
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-04-30 12:50:42 +0530

    print output

---
+++ TakingOutput.java
@@ -0,0 +1,15 @@
+// Lecture 2 - Input/Output in java
+public class TakingOutput {
+    public static void main(String[] args) {
+        System.out.println("Hello piyush");
+        System.out.println("Hello piyush");
+        System.out.println("Hello piyush");
+
+        // Print Pattern
+
+         System.out.println("****");
+         System.out.println("***");
+         System.out.println("**");
+         System.out.println("*");
+    }
+}
