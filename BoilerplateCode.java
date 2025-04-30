ddb3d79f94ff7fe39b0aaadaa6858c3437605129
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-05-01 00:36:43 +0530

    How code is actually work

--- BoilerplateCode.java
+++ BoilerplateCode.java
@@ -8,3 +8,7 @@ public class BoilerplateCode {
 // Basic Terms 
 // ; -> Terminator, which is must to end the statement
 //  -> This is comment, which is not be included as code.
+
+// How is our code running 
+
+// Compilation [ Source Code(.java) -> Compiler -> Byte code(.class) ] -> Execution [ Java Virtual machine(JVM) -> Native code ] 
