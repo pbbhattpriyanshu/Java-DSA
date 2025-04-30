5f2283cee42d07686b8bb9e8cf0eeb35b99066b1
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-04-30 23:31:14 +0530

    taking input

--- TakingOutput.java
+++ TakingOutput.java
@@ -1,15 +1,58 @@
 // Lecture 2 - Input/Output in java
+
+import java.util.*;
+
 public class TakingOutput {
     public static void main(String[] args) {
-        System.out.println("Hello piyush");
-        System.out.println("Hello piyush");
-        System.out.println("Hello piyush");
+        // System.out.println("Hello piyush");
+        // System.out.println("Hello piyush");
+        // System.out.println("Hello piyush");
 
-        // Print Pattern
+        // // Print Pattern
 
-         System.out.println("****");
-         System.out.println("***");
-         System.out.println("**");
-         System.out.println("*");
+        //  System.out.println("****");
+        //  System.out.println("***");
+        //  System.out.println("**");
+        //  System.out.println("*");
+
+        // Input in java
+
+        // Scanner sc = new Scanner(System.in);
+        // String input = sc.next();     // print single words only
+        // System.out.println(input); 
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+        // float price = sc.nextFloat();
+        // System.out.println(price);
+
+        // Example 
+
+        // Sum of a & b by taking input
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // System.out.println("The Sum is " + sum);
+        // sc.close();
+
+        // Product of a & b by taking input
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int Product = a * b;
+        // System.out.println("The prduct is " + Product);
+        // sc.close();
+
+        // Area of Circle
+        // Scanner sc = new Scanner(System.in);
+        // float radii = sc.nextFloat();
+        // float Ar = 3.14f * radii * radii;
+        // System.out.println(Ar);
+        // sc.close();
     }
 }
