ed8e3ccba1aea8fc089e56c6600c67c51b658316
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-04-30 22:51:11 +0530

    data type in java

---
+++ Datatypes.java
@@ -0,0 +1,46 @@
+// Lecture 4 - Data Types in java
+public class Datatypes {
+    public static void main(String[] args) {
+        // There are two types of datatype
+        // Java is typed language where you tell first what is your data type before creating any variable
+
+        // 1. Primitive (Pre-defined)
+        /*
+         * byte  
+         * short
+         * char
+         * boolean
+         * int
+         * long         much greater value integer/number
+         * float
+         * double       much grater value float/decimal
+         */
+        // Examples 
+
+        // byte a = 22;                // 0 < a < 256
+        // System.out.println(a);
+        // char pb = 'p';              // Single Characters only
+        // System.out.println(pb);
+        // boolean var = true;         // true of false 
+        // System.out.println(var);
+        // float price = 77;             // decimal value
+        // System.out.println(price);
+ 
+         // 2. Non-Primitive (define by user)
+           /*
+         * String
+         * Array
+         * Class
+         * Object
+         * Interface
+         */
+
+         // Example 
+
+         // Sum of a & b
+         int a = 23;
+         int b = 32;
+         int sum = a + b;
+         System.out.println(sum);
+    }
+}
