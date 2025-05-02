dc7d0b996471a6351dfec6770cb20970db82ff3b
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-05-02 18:00:32 +0530

    for loop

--- loops.java
+++ loops.java
@@ -13,17 +13,42 @@ public class loops {
 
         // Print sum from 1 to n  (n given by user)
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Number");
-        int num = sc.nextInt();
-        int i = 1;
-        int sum = 0;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the Number");
+        // int num = sc.nextInt();
+        // int i = 1;
+        // int sum = 0;
 
-        while (i <= num) {
-            sum = sum + i;
-            i++;
-        }
-        System.out.println("The loop is end and Sum is : "+ sum);
-        sc.close(); 
+        // while (i <= num) {
+        //     sum = sum + i;
+        //     i++;
+        // }
+        // System.out.println("The loop is end and Sum is : "+ sum);
+        // sc.close(); 
+
+        // for loop
+        // for(initialisation; condition; updation) {
+        //     // work
+        // }
+
+        // for(int i = 1; i<=5; i++){
+        //    System.out.println("Hello World");
+        // }
+
+        // Print Square Print
+        // for(int i = 1; i<=4; i++){
+        //     System.out.println("****");
+        // }
+
+        // Print reverse of a number 10899
+
+        // int n = 10899;
+        // int rev = 0;
+        // while (n > 0) {
+        //     int lastDigit = n % 10;
+        //     rev = (rev *10) + lastDigit;
+        //     n = n / 10;
+        // }
+        // System.out.println(rev );
     }
 }
