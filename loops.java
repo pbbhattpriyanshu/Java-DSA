6d5ad09fb08e3c8ea4a1d2d1b7ae459299187b4e
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-05-04 22:49:47 +0530

    loop is finished

--- loops.java
+++ loops.java
@@ -161,6 +161,26 @@ public class loops {
         //     System.out.println("your number was : " +num);
         // } while (true);
 
-        
+        // Check if a number is prime or not
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        if (n == 2) {
+            System.out.println("n is prime");
+        } else {
+            boolean isPrime = true;
+            for(int i=2; i<=Math.sqrt(n); i++){
+                if (n % i == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime == true) {
+                System.out.println("n is prime");
+            } else {
+                System.out.println("n is not prime");
+            }
+        }
+
     }
 }
