a24df17cda9178ad08474613277f7ce3684140ce
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-05-19 07:09:29 +0530

    decimal to binary

--- Functions.java
+++ Functions.java
@@ -85,19 +85,35 @@ public class Functions {
     // }
 
     //Convert from binary to decimal
-    public static void binToDec(int binNum) {
-        int myNum = binNum;
-        int pow = 0;
-        int decNum = 0;
+    // public static void binToDec(int binNum) {
+    //     int myNum = binNum;
+    //     int pow = 0;
+    //     int decNum = 0;
 
-        while (binNum > 0) {
-            int lastDigit = binNum % 10;
-            decNum = decNum + (lastDigit * (int)Math.pow(2, pow));
+    //     while (binNum > 0) {
+    //         int lastDigit = binNum % 10;
+    //         decNum = decNum + (lastDigit * (int)Math.pow(2, pow));
+
+    //         pow++;
+    //         binNum = binNum/10;
+    //     }
+    //     System.out.println("decimal of " + myNum +" = " + decNum);
+    // }
+
+    //Convert from decimal to binary
+    public static void decToBin(int n){
+        int myNum = n;
+        int pow = 0;
+        int binNum = 0;
+
+        while (n > 0) {
+            int rem = n % 2;
+            binNum = binNum + (rem * (int)Math.pow(10, pow));
 
             pow++;
-            binNum = binNum/10;
+            n = n/2;
         }
-        System.out.println("decimal of " + myNum +" = " + decNum);
+        System.out.println("binary form of " + myNum + " = " + binNum);
     }
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
@@ -122,7 +138,7 @@ public class Functions {
     // Call by reference --> original value
 
     // primeInRange(10);
-    binToDec(101);
+    decToBin(5);
     } 
 }
 
