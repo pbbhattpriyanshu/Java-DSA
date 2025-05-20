0d0bcc1b7ee4689545db4d0a3cd3b94bdb94e3df
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-05-20 19:24:23 +0530

    0-1 triangle

--- Pattern02.java
+++ Pattern02.java
@@ -58,18 +58,21 @@ public class Pattern02 {
 // }
 
 // Floyd's Triangle
-public static void floyd(int rows){
-    int num = 1;
-    //outer loop
-    for(int i=1; i<=rows; i++){
-        //inner loop
-        for(int j=1; j<=i; j++){
-            System.out.print(num+" ");
-            num++;
-        }
-        System.out.println();
-    }
-}
+// public static void floyd(int rows){
+//     int num = 1;
+//     //outer loop
+//     for(int i=1; i<=rows; i++){
+//         //inner loop
+//         for(int j=1; j<=i; j++){
+//             System.out.print(num+" ");
+//             num++;
+//         }
+//         System.out.println();
+//     }
+// }
+
+// 0-1 Triangle
+
     public static void main(String[] args) {
         floyd(5);
     }
