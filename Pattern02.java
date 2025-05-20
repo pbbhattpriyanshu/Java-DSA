f89fd3833e4bce03cb5c4e0598f3a3f71f4ac299
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-05-20 18:39:14 +0530

    Print Inverted & Rotated Half-Pyramid

--- Pattern02.java
+++ Pattern02.java
@@ -1,21 +1,38 @@
 public class Pattern02 {
     //Print Hollow-Rectangle Pattern
-    public static void hollow_rectangle(int totRows, int totCols){
-        //outer loop
-        for(int i =1; i<=totRows; i++){
-            //inner
-            for(int j=1; j<=totCols; j++){
-                if (i == 1 || i == totRows || j == 1 || j == totCols) {
-                    //boundary cells
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+    // public static void hollow_rectangle(int totRows, int totCols){
+    //     //outer loop
+    //     for(int i =1; i<=totRows; i++){
+    //         //inner
+    //         for(int j=1; j<=totCols; j++){
+    //             if (i == 1 || i == totRows || j == 1 || j == totCols) {
+    //                 //boundary cells
+    //                 System.out.print("*");
+    //             } else {
+    //                 System.out.print(" ");
+    //             }
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    //Print Inverted & Rotated Half-Pyramid
+   public static void inpyramid(int n) {
+    //outer loop
+    for(int i=1; i<=n; i++){
+        //inner loop
+        for(int j=1; j<=n-i; j++){
+            System.out.print(" ");
         }
+        //inner loop (star)
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
     }
+   }
     public static void main(String[] args) {
-        hollow_rectangle(4, 5);
+        
+        inpyramid(4);
     }
 }
