c957c1afd2a28319a4d5d7eba91537d50811dc61
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-05-20 19:08:29 +0530

    Half-pyramid with numbers

--- Pattern02.java
+++ Pattern02.java
@@ -45,7 +45,17 @@ public class Pattern02 {
 //     }
 // }
 
-
+// Half-pyramid with numbers
+public static void numpyramid(int num){
+    //outer loop
+    for(int i=1; i<=num; i++){
+        //inner loop
+        for(int j=1; j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
     public static void main(String[] args) {
         
         numpyramid(5);
