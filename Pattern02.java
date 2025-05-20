1648bfbc9e6134d572f8fe6d763bc68804b2660c
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-05-20 19:01:01 +0530

    Invverted half-pyramid with numbers

--- Pattern02.java
+++ Pattern02.java
@@ -16,23 +16,38 @@ public class Pattern02 {
     //     }
     // }
 
-    //Print Inverted & Rotated Half-Pyramid
-   public static void inpyramid(int n) {
-    //outer loop
-    for(int i=1; i<=n; i++){
-        //inner loop
-        for(int j=1; j<=n-i; j++){
-            System.out.print(" ");
-        }
-        //inner loop (star)
-        for(int j=1; j<=i; j++){
-            System.out.print("*");
-        }
-        System.out.println();
-    }
-   }
+   //Print Inverted & Rotated Half-Pyramid
+//    public static void inpyramid(int n) {
+//     //outer loop
+//     for(int i=1; i<=n; i++){
+//         //inner loop
+//         for(int j=1; j<=n-i; j++){
+//             System.out.print(" ");
+//         }
+//         //inner loop (star)
+//         for(int j=1; j<=i; j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//   }
+
+
+// Invverted half-pyramid with numbers
+// public static void numpyramid(int num){
+//     //outer loop
+//     for(int i=1; i<=num; i++){
+//         //inner loop
+//         for(int j=1; j<=num+i-2; j++){
+//             System.out.print(j);
+//         }
+//         System.out.println();
+//     }
+// }
+
+
     public static void main(String[] args) {
         
-        inpyramid(4);
+        numpyramid(5);
     }
 }
