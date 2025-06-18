533a2bdc98470e0c59f503c319c10ad373275ff8
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-06-18 22:46:07 +0530

    revision

--- BoilerplateCode.java
+++ BoilerplateCode.java
@@ -3,12 +3,4 @@ public class BoilerplateCode {
     public static void main(String[] args) {
         // main content or code which excecute   
     }
-}
-
-// Basic Terms 
-// ; -> Terminator, which is must to end the statement
-//  -> This is comment, which is not be included as code.
-
-// How is our code running 
-
-// Compilation [ Source Code(.java) -> Compiler -> Byte code(.class) ] -> Execution [ Java Virtual machine(JVM) -> Native code ] 
+};
---
+++ Revision.java
@@ -0,0 +1,9 @@
+public class Revision {
+    public static void main(String[] args) {
+
+        byte a = 75;
+        byte b = 75;
+
+        System.out.println(a+b);
+    }
+}
