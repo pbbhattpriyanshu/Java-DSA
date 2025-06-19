86a030a72e0be0847059ac21055b4cfda8775fc9
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-06-19 17:02:39 +0530

    Income tax calcu

---
+++ Mini Project/TaxCal.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class TaxCal {
+    public static void main(String[] args) {
+        
+        // Income Tax Calculator
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please Enter your Income: ");
+        int income = sc.nextInt();
+        float per20 = income * (0.2f);
+        float per30 = income * (0.3f);
+        if (income < 700000) {
+            System.out.println("Tax is Zero");
+        }
+        else if (income >= 700000 && income <= 1400000) {
+            System.out.println("Your Tax is : " + per20);
+        }
+        else {
+            System.out.println("Your Tax is : " + per30);
+        }
+        System.out.println("Thank you, Please visit again");  
+
+
+        sc.close();
+    }
+}
--- Revision.java
+++ Revision.java
@@ -1,9 +1,16 @@
 import java.util.*;
 public class Revision {
     public static void main(String[] args) {
-        // 
+        // if/else
         Scanner sc = new Scanner(System.in);
-        
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        if (age >= 18) {
+            System.out.println("You are Eligible");
+        } else {
+            System.out.println("Not Eligible");
+        }
         sc.close();
 
     }
