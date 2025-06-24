6b50edb18ca17178118f2c8a92d923409887f10a
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-06-24 07:48:58 +0530

    Discount calculator

--- Mini Project/discountCalc.java
+++ Mini Project/discountCalc.java
@@ -4,6 +4,18 @@ public class discountCalc {
     public static void main(String[] args) {
         // Discount calculator
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the orignal price: ");
+        double orignalPrice = sc.nextDouble();
+
+        System.out.print("Enter the discount in % (like 23):  ");
+        double discountPercent = sc.nextDouble();
+
+        double discountAmount = (discountPercent / 100) * orignalPrice;
+        double finalPrice = orignalPrice - discountAmount;
+
+        System.out.println("After Discount, You pay: " +finalPrice);
+        System.out.println("You'r saving: " + discountAmount);
         sc.close();
     }
 }
