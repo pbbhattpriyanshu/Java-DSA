c99e00683c937a195ae97cbff9f2e286c0637398
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-06-24 12:51:03 +0530

    Ternary operator

---
+++ Ternaryop.java
@@ -0,0 +1,19 @@
+public class Ternaryop {
+    public static void main(String[] args) {
+        // ternary operator ? = Return 1 or 2 values, if condition is true
+
+        // variable = (condition) ? ifTrue : ifFalse;
+
+        // int score = 76;
+
+        // String cutoff = (score >= 83) ? "QUALIFY" : "DISQUALIFY";
+        // System.out.println(cutoff);
+
+        // int num = 31;
+
+        // String oddOrEven = (num % 2 == 0) ? "EVEN" : "ODD";
+        // System.out.println(oddOrEven);
+
+        
+    }
+}
