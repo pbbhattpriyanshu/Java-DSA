a3b2af253165c5dbf8e67cbf1ee401ae9afc0f70
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-06-24 15:52:35 +0530

    Priority Queue

--- DSA/queue.java
+++ DSA/queue.java
@@ -1,6 +1,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
+
 public class queue {
     public static void main(String[] args) {
         Queue<String> queue = new LinkedList<String>();
