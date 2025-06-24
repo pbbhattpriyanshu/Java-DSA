11ad28dc29a6cee5bfe0adc60e38fa7ac61162fe
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-06-24 08:07:23 +0530

    String method and substring

--- Revision.java
+++ Revision.java
@@ -1,27 +1,6 @@
 import java.util.*;
 public class Revision {
     public static void main(String[] args) {
-        // if/else
-        Scanner sc = new Scanner(System.in);
-
-        boolean isStudent = true;
-        boolean isAdult;
-        // double discountPercent = 30;
-
-        System.out.print("Enter the actual cost: ");
-        double price = sc.nextDouble();
         
-
-        if (isStudent) {
-            double discountAmount = (75.0 / 100) * price;
-            double finalPrice = price - discountAmount;
-            System.out.printf("Final price after discount: %.2f\n", finalPrice);
-
-        } else {
-            double discountAmount = (15.0 / 100) * price;
-            double finalPrice = price - discountAmount;
-            System.out.printf("Final price after discount: %.2f\n", finalPrice);
-        }
-        sc.close();
     }
 }
---
+++ Str.java
@@ -0,0 +1,13 @@
+public class Str {
+    public static void main(String[] args) {
+        // String method and substring 
+        String name = "Piyush bhatt";
+
+        System.out.println(name.length());
+        System.out.println(name.charAt(6));
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.replace("i", "e"));
+        System.out.println(name.substring(0, 6));
+    }
+}
