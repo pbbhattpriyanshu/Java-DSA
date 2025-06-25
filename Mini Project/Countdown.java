44292d4ff05a35eabbc32a9478803a47563419a0
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-06-25 22:42:13 +0530

    Countdown app

---
+++ Mini Project/Countdown.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Countdown {
+    public static void main(String[] args) {
+        // Countdown app
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the starting number for countdown: ");
+        int max = sc.nextInt();
+
+        for(int i = max; i>=0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000); // Pause for 1 second
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        } 
+
+        System.out.println("BOOM!💥💥");
+
+        sc.close();
+    }
+}
