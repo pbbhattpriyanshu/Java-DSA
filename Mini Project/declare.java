cbcf406b434baa108f5892dcc0a15bbf44484b6b
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-06-26 20:09:28 +0530

    Scope variable - class, local, block

--- Mini Project/declare.java
+++ Mini Project/declare.java
@@ -3,13 +3,17 @@ public class declare {
 
     public static void main(String[] args) {
         int y = 43; // Local variable to main
+        int x = 21;
+
+        System.out.println(x); // 21 -- Local scope
 
         System.out.println(x + y); // Prints 3 + 43 = 46
 
         {
             int z = 34; // z is declared inside this block and is only accessible here
+    
         }
 
-        System.out.println(x + y + z); // ERROR: z is out of scope here
+        // System.out.println(x + y + z); // ERROR: z is out of scope here
     }
 }
