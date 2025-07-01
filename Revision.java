af63423a380275a569090f0a5ec0eac6773bc550
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-07-01 17:16:32 +0530

    ArrayList - DSA

--- Revision.java
+++ Revision.java
@@ -1,7 +1,17 @@
 import java.util.*;
+import java.util.Arrays;
 public class Revision {
     public static void main(String[] args) {
+        // arrays
+        String fruits[] = {"apple", "mango", "pees", "orange"};
 
+            String nameP = "Piyush, Kajal";
+            System.out.println(nameP);
+
+            String name [] = {"Piyush", "Kajal"};
+            String namek =     name[1];
+            // System.out.println(name[1]);
+            System.out.println(namek);
     }
 }
  
