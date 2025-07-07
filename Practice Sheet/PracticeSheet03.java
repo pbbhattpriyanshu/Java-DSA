653dd7c1d0b707d9a2992ae5b4f471336f9f9a68
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-07-07 22:54:03 +0530

    sum of Pair of an  array

--- Practice Sheet/PracticeSheet03.java
+++ Practice Sheet/PracticeSheet03.java
@@ -26,28 +26,71 @@ public class PracticeSheet03 {
         // Rotate the array
 
         // RIGHT ROTATION
-        Scanner sc = new Scanner(System.in);
-        String[] name = {"Piyush", "Pankaj", "Priyanshu", "Pavan"};
-        System.out.print("How many times rotation is done: ");
-        int k = sc.nextInt(); // Number of times to rotate right
+        // Scanner sc = new Scanner(System.in);
+        // String[] name = {"Piyush", "Pankaj", "Priyanshu", "Pavan"};
+        // System.out.print("How many times rotation is done: ");
+        // int k = sc.nextInt(); // Number of times to rotate right
        
-        for (int r = 0; r <  k % name.length; r++) {
-            String temp = name[name.length - 1]; // Save last element
-            for (int j = name.length - 1; j > 0; j--) {
-                name[j] = name[j - 1]; // Shift each element right
+        // for (int r = 0; r <  k % name.length; r++) {
+        //     String temp = name[name.length - 1]; // Save last element
+        //     for (int j = name.length - 1; j > 0; j--) {
+        //         name[j] = name[j - 1]; // Shift each element right
+        //     }
+        //     System.out.println(name[1]);
+        //     name[0] = temp; // Place last element at the front
+        // }
+
+        // // Print the rotated array
+        // for (int i = 0; i < name.length; i++) {
+        //     System.out.print(name[i] + " ");
+        // }
+        // System.out.println();
+
+        // sc.close();
+
+        // int [] arr = {1,3,-2,6};
+        // int max = Integer.MIN_VALUE;
+        // int min = Integer.MAX_VALUE;
+
+        // for(int i=0; i<arr.length; i++){
+        //     if (max < arr[i]) {
+        //         max = arr[i];
+        //     }
+        //     if (min > arr[i]) {
+        //         min = arr[i];
+        //     }
+        // } System.out.println("Max value: " + max); // 6
+        //  System.out.println("Min value: " + min); // -2
+        
+        // int countMin = 0;
+        // int countMax = 0;
+        // for(int j = 0; j<arr.length; j++){
+        //     if (arr[j] == max) {
+        //         countMax++;                
+        //     }
+        //     if (arr[j] == min) {
+        //         countMin++;                
+        //     }
+        // }
+
+        // int validMin = arr.length - countMax;
+        // int validMax = arr.length - countMin;
+        // System.out.println("Valid Minimum numbers: " + validMin); //3
+        // System.out.println("Valid Maximum numbers: " + validMax); //3
+
+        // Pair of an array
+
+        int [] arr = {3,5,2,7,5}; // 4
+        int k = 12;
+        int n = 3;
+
+        for(int i=0; i<=n; i++) {
+            for(int j=i+1; j<=n+1; j++) {
+                if (arr[i]+ arr[j] == k) {
+                    System.out.println("Sum is found: " + "(" + i + "," + j + ")" );
+                }
             }
-            System.out.println(name[1]);
-            name[0] = temp; // Place last element at the front
         }
-
-        // Print the rotated array
-        for (int i = 0; i < name.length; i++) {
-            System.out.print(name[i] + " ");
-        }
-        System.out.println();
-
-        sc.close();
-
 
     }
 }
--- Practice Sheet/PracticeSheet2.java
+++ Practice Sheet/PracticeSheet2.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class PracticeSheet2 {
     public static void main(String[] args) {
         //Arrays
-        // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        // System.out.print("Enter the length: ");
-        // int n = sc.nextInt();
-        // int number[] = new int[n];
+        System.out.print("Enter the length: ");
+        int n = sc.nextInt();
+        int number[] = new int[n];
 
         // n == number.length
         // number = 3,2,7,6,5
 
         // input the values
-        // for(int i=0; i < n; i++) {
-        //     System.out.print("Enter the values: ");
-        //     number[i] = sc.nextInt();
-        // };
+        for(int i=0; i < n; i++) {
+            System.out.print("Enter the values: ");
+            number[i] = sc.nextInt();
+        };
        
         // sum of an array
         // int sum = 0;
@@ -26,13 +26,13 @@ public class PracticeSheet2 {
         // System.out.println("Sum of an Array: " + sum);
 
         // max value in array
-        // int max = 0;
-        // for(int i=0; i < n; i++) {
-        //     if (number[i] > max) {
-        //         max = number[i];
-        //     }
-        // };
-        // System.out.println("Max value: " + max);
+        int max = 0;
+        for(int i=0; i < n; i++) {
+            if (number[i] > max) {
+                max = number[i];
+            }
+        };
+        System.out.println("Max value: " + max);
 
         // min value in array
         // int min = Integer.MIN_VALUE;
@@ -45,29 +45,29 @@ public class PracticeSheet2 {
 
         // Swap an array
 
-        int [] arr = {10,20,30,40,50,60,70,80,90};  // index - 8
-        int sp = 0;
-        int fp = arr.length-1;
+        // int [] arr = {10,20,30,40,50,60,70,80,90};  // index - 8
+        // int sp = 0;
+        // int fp = arr.length-1;
 
-        swapArr(arr, sp, fp);
+        // swapArr(arr, sp, fp);
 
-        // Print the reversed array
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        // // Print the reversed array
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i] + " ");
+        // }
 
         
 
         // sc.close();
 
     }
-    public static void swapArr(int [] arr,int sp, int fp) {
-        while (sp < fp) {
-            int temp = arr[sp];
-            arr[sp] = arr[fp];
-            arr[fp] = temp;
-            sp++;
-            fp--;
-        }
-    }
+    // public static void swapArr(int [] arr,int sp, int fp) {
+    //     while (sp < fp) {
+    //         int temp = arr[sp];
+    //         arr[sp] = arr[fp];
+    //         arr[fp] = temp;
+    //         sp++;
+    //         fp--;
+    //     }
+    // }
 }
