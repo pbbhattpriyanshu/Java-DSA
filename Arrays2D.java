d84a8493186b4bf35255a88bc380a8f63f8fbb15
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-07-09 07:58:54 +0530

    2D array - wave form

--- Arrays2D.java
+++ Arrays2D.java
@@ -14,14 +14,24 @@ public class Arrays2D {
 
         int [][] arr = new int[n][m];
 
-        System.out.println("Before");
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                System.out.print(arr[i][j] + " "); 
-            }
-            System.out.println();
-        }
+        // System.out.println("Before");
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         System.out.print(arr[i][j] + " "); 
+        //     }
+        //     System.out.println();
+        // }
 
+        //col-wise
+        // for(int j=0; j<n; j++) {
+        //     for(int i=0; i<m; i++) {
+        //         System.out.print("Enter the values: ");
+        //         arr[i][j] = sc.nextInt();
+        //     }
+        //     System.out.println();
+        // }
+
+        //row-wise
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 System.out.print("Enter the values: ");
@@ -29,7 +39,8 @@ public class Arrays2D {
             }
             System.out.println();
         }
-        System.out.println("after");
+
+        System.out.println("after/normal form R TO L");
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 System.out.print(arr[i][j] + " "); 
@@ -37,6 +48,23 @@ public class Arrays2D {
             System.out.println();
         }
 
-      
+        //print given array in wave form
+
+        System.out.println("Wave form");
+        for(int i=0; i<n; i++) {
+            if (i%2==0) {
+                for(int j=0; j<m; j++){
+                    System.out.print(arr[i][j] + " ");
+                } 
+            }
+            else {
+                for(int j=m-1; j>=0; j--){
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+      sc.close();
     }
 }
