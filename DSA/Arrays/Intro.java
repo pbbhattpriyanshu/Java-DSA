7c1c9ee4dc72e0e8269be151d9073cac57971b1c
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-08-05 15:39:13 +0530

    Added methods for sum, average, and swapping array elements, and commented out code for array creation and input from user.

--- DSA/Arrays/Intro.java
+++ DSA/Arrays/Intro.java
@@ -3,56 +3,66 @@ package DSA.Arrays;
 import java.util.Scanner;
 
 public class Intro {
-    //Sum of an Array
-    public static void sum(int arr[]){
+    // Sum of an Array
+    public static void sum(int arr[]) {
         int sum = 0;
-        for(int i=0; i<arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
         }
         System.out.println("Sum of an Array is " + sum);
     }
 
-    //Average of an Array
-    public static void avg(int arr[]){
+    // Average of an Array
+    public static void avg(int arr[]) {
         int sum = 0;
-        for(int i=0; i<arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
         }
-        int avg = sum/arr.length;
+        int avg = sum / arr.length;
         System.out.println("Average of an Array is " + avg);
     }
+
+    // Swap the values
+    public static void swap(int arr[]) {
+        int temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp;
+
+        System.out.println("After Swaping index 0 = " + arr[0] + " and index 1 = " + arr[1]);
+    }
+
     public static void main(String[] args) {
-       // Syntax - type [] name = new type [size]
+        // Syntax - type [] name = new type [size]
 
-       int [] ary = {1,2,3,4,5,6,7};
+        int[] ary = { 1, 2, 3, 4, 5, 6, 7 };
 
-       ary[3] = 23; //mutate
+        ary[3] = 23; // mutate
 
-       //total length
-       //System.out.println("length of array: " + ary.length);
+        // total length
+        // System.out.println("length of array: " + ary.length);
 
-       //print
-    //    for(int i=0; i<ary.length; i++){   
-    //     System.out.println(ary[i]);
-    //    };
+        // print
+        // for(int i=0; i<ary.length; i++){
+        // System.out.println(ary[i]);
+        // };
 
-       //Create an by taking Input 
-       Scanner sc = new Scanner(System.in);
-       System.out.print("Enter Size of an arry: ");
-       int n = sc.nextInt();
-       int [] arr = new int[n];
+        // Create an by taking Input
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Size of an arry: ");
+        // int n = sc.nextInt();
+        // int[] arr = new int[n];
 
-       //Access every index to take input
-       for(int i=0; i<n; i++){
-        System.out.print("Enter the value in " + i + " index: ");
-        arr[i] = sc.nextInt();
-       }
+        // Access every index to take input
+        // for (int i = 0; i < n; i++) {
+        //     System.out.print("Enter the value in " + i + " index: ");
+        //     arr[i] = sc.nextInt();
+        // }
 
-       //Call
-       sum(arr);
-       avg(arr);
+        // Call
+        // sum(arr);
+        // avg(arr);
 
-       sc.close();
+        //sc.close();
 
     }
 }
