8a99bda9d6caac2cbe6b30141706eb76af16c51f
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-08-15 13:30:39 +0530

    Find max element

--- Practice Sheet/ArraySheet.java
+++ Practice Sheet/ArraySheet.java
@@ -10,9 +10,26 @@ public class ArraySheet {
         System.out.print("]");
     }
 
+    // 2  Find max element   Traversal     Easy    Min/Max logic
+    public static void max(int arr[]){
 
+        // assumption: index 0 has the max element initially
+        int maxElement = arr[0];
+        int maxIndex = 0;
+
+        // traverse the array
+        for(int i=1; i<arr.length; i++){
+            if (maxElement < arr[i]) {
+                maxElement = arr[i];  
+                maxIndex = i; // update index when a new max is found              
+            }
+        }
+
+        // print max element & index
+        System.out.println("Max Element: " + maxElement + " at index " + maxIndex);
+    }
     public static void main(String[] args) {
-        int arr [] = {1,3,4,5,6,8,9,4,2};
-        print(arr);
+        int arr [] = {1,3,4,5,6,8,19,4,2,8};
+        max(arr);
     }
 }
