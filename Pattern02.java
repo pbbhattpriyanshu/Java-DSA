6d39110c0791a6465fecce167deaa4c7963d96b9
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-09-09 07:59:49 +0530

    Update

--- Pattern02.java
+++ Pattern02.java
@@ -1,75 +1,75 @@
 public class Pattern02 {
     //Print Hollow-Rectangle Pattern
-    // public static void hollow_rectangle(int totRows, int totCols){
-    //     //outer loop
-    //     for(int i =1; i<=totRows; i++){
-    //         //inner
-    //         for(int j=1; j<=totCols; j++){
-    //             if (i == 1 || i == totRows || j == 1 || j == totCols) {
-    //                 //boundary cells
-    //                 System.out.print("*");
-    //             } else {
-    //                 System.out.print(" ");
-    //             }
-    //         }
-    //         System.out.println();
-    //     }
-    // }
+    public static void hollow_rectangle(int totRows, int totCols){
+        //outer loop
+        for(int i =1; i<=totRows; i++){
+            //inner
+            for(int j=1; j<=totCols; j++){
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    //boundary cells
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
    //Print Inverted & Rotated Half-Pyramid
-//    public static void inpyramid(int n) {
-//     //outer loop
-//     for(int i=1; i<=n; i++){
-//         //inner loop
-//         for(int j=1; j<=n-i; j++){
-//             System.out.print(" ");
-//         }
-//         //inner loop (star)
-//         for(int j=1; j<=i; j++){
-//             System.out.print("*");
-//         }
-//         System.out.println();
-//     }
-//   }
+   public static void inpyramid(int n) {
+    //outer loop
+    for(int i=1; i<=n; i++){
+        //inner loop
+        for(int j=1; j<=n-i; j++){
+            System.out.print(" ");
+        }
+        //inner loop (star)
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+  }
 
 
 // Invverted half-pyramid with numbers
-// public static void numpyramid(int num){
-//     //outer loop
-//     for(int i=1; i<=num; i++){
-//         //inner loop
-//         for(int j=1; j<=num+i-2; j++){
-//             System.out.print(j);
-//         }
-//         System.out.println();
-//     }
-// }
+public static void numpyramid1(int num){
+    //outer loop
+    for(int i=1; i<=num; i++){
+        //inner loop
+        for(int j=1; j<=num+i-2; j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+}
 
 // Half-pyramid with numbers
-// public static void numpyramid(int num){
-//     //outer loop
-//     for(int i=1; i<=num; i++){
-//         //inner loop
-//         for(int j=1; j<=i;j++){
-//             System.out.print("*");
-//         }
-//         System.out.println();
-//     }
-// }
+public static void numpyramid(int num){
+    //outer loop
+    for(int i=1; i<=num; i++){
+        //inner loop
+        for(int j=1; j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
 
 // Floyd's Triangle
-// public static void floyd(int rows){
-//     int num = 1;
-//     //outer loop
-//     for(int i=1; i<=rows; i++){
-//         //inner loop
-//         for(int j=1; j<=i; j++){
-//             System.out.print(num+" ");
-//             num++;
-//         }
-//         System.out.println();
-//     }
-// }
+public static void floyd(int rows){
+    int num = 1;
+    //outer loop
+    for(int i=1; i<=rows; i++){
+        //inner loop
+        for(int j=1; j<=i; j++){
+            System.out.print(num+" ");
+            num++;
+        }
+        System.out.println();
+    }
+}
 
 // Print the star pattern
 public static void starRectangle(int row, int col){
@@ -82,7 +82,19 @@ public static void starRectangle(int row, int col){
         System.out.println();
     }
 }
+// Print the star pattern
+public static void numberSq(int num){
+    //outer loop
+    for(int i=1; i<=num; i++){
+        //inner loop
+        for(int j=1; j<=num; j++){
+            System.out.print(j + " ");
+
+        }
+        System.out.println();
+    }
+}
     public static void main(String[] args) {
-        starRectangle(4,4);
+        numberSq(5);
     }
 }
