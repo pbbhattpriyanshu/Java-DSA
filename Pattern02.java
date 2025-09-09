684c4bf11f6666158ed6bf87fba9df811a10546d
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-09-09 07:34:29 +0530

    `Refactored starRectangle method: added space after stars, adjusted loop condition in inner for loop.`

--- Pattern02.java
+++ Pattern02.java
@@ -75,14 +75,14 @@ public class Pattern02 {
 public static void starRectangle(int row, int col){
     //outer loop
     for(int i=1; i<=row; i++){
-        System.out.print("*");
-        for (int j=1; j<= col; j++) {
-            System.out.print("*");
+        System.out.print("* ");
+        for (int j=1; j < col; j++) {
+            System.out.print("* ");
         }
         System.out.println();
     }
 }
     public static void main(String[] args) {
-        starRectangle(3,5);
+        starRectangle(4,4);
     }
 }
