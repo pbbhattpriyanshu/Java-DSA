6524d4d86ff71315d05f71d3e12bfb4cc2cea9c5
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-10-29 11:29:08 +0530

    "Deleted file a/Pattern02.class, now empty"

---
+++ Practice_Sheet/PracticeSheet06.java
@@ -0,0 +1,15 @@
+//Pattern -  Concepts
+public class PracticeSheet06 {
+    public static void main(String[] args) {
+        //1. Box Strategy 
+        //No. of rows and col are same
+
+        int n = 5;
+        for(int row=1; row<=n; row++){
+            for(int col=1; col<=n; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
