6ea35749763a78a7c05b1810b84e980852592f10
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-11-26 11:58:52 +0530

    `Updated SearchWithSortedCheck.java and linearSearch.java with minor changes to test cases.`

--- DSA/Arrays/linearSearch.java
+++ DSA/Arrays/linearSearch.java
@@ -10,12 +10,13 @@ public class linearSearch{
 
         //traverse 
         for(int i=0; i<arr.length; i++){
+            count++;
             //condition for target
             if (arr[i] == target) {
                 System.out.println("At index: " + i);
                 isFound = true;
+                break;
             }
-            count++;
         }
 
         //condition for output
