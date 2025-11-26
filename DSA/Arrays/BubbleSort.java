1eecd1c6eec3c85ee160d2c1603b6db97826fafa
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-11-26 22:20:10 +0530

    Add BubbleSort implementation with sorting and printing functionality

---
+++ DSA/Arrays/BubbleSort.java
@@ -0,0 +1,29 @@
+public record BubbleSort() {
+    //Bubble Sort
+    public static void bbleSort(int arr[], int n){
+        //Phase loop
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-1-i; j++){
+                //logic
+                if (arr[j]>arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        //Print Array
+        System.out.println("Sorted Array is: ");
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int []arr = {2,3,4,1};
+        int n = arr.length;
+
+        bbleSort(arr, n);
+
+    }
+}
