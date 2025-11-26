d40186c210eafeb57f7e8b8df00c64d366bdd7bc
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-11-26 22:33:59 +0530

    Added comment to clarify sorting logic in BubbleSort in descending order implementation

--- DSA/Arrays/BubbleSort.java
+++ DSA/Arrays/BubbleSort.java
@@ -5,7 +5,7 @@ public record BubbleSort() {
         for(int i=0; i<n-1; i++){
             for(int j=0; j<n-1-i; j++){
                 //logic
-                if (arr[j]>arr[j+1]) {
+                if (arr[j]>arr[j+1]) { //descending order convert sign  > / < 
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
