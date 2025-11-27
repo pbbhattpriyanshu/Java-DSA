314bde8394909aece302a32b947320c804e88b11
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-11-27 19:48:06 +0530

    `Changed comparison operator in SelectionSort algorithm from < to > for descending order`

--- DSA/Arrays/SelectionSort.java
+++ DSA/Arrays/SelectionSort.java
@@ -4,7 +4,7 @@ public class SelectionSort {
         for(int i=0; i<n-1; i++){
             int minIndex = i;
             for(int j=i+1; j<n; j++){
-                if (arr[minIndex] < arr[j]) {   //descending order convert sign  > / < 
+                if (arr[minIndex] > arr[j]) {   //descending order convert sign  > / < 
                     minIndex = j;
                 }
             }
