698c0bb0b8e822b956b7b536e4f40e91e6365faf
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-12-06 22:28:13 +0530

    Add removeDublicate method to eliminate duplicates from a sorted array and update main method for sorting check

--- Practice_Sheet/PracticeSheet07.java
+++ Practice_Sheet/PracticeSheet07.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class PracticeSheet07 {
     // Reverse loop
     public static void revrse(int[] arr, int n) {
@@ -164,11 +166,61 @@ public class PracticeSheet07 {
         }
     }
 
+    // Remove Duplicates from Sorted Array
+    public static void removeDublicate(int arr[]){
+        //print before remove dublicate element
+        System.out.println("before remove dublicate element Array " + Arrays.toString(arr));
+        //slow pointer
+        int j = 0;
+
+        //traverse
+        for(int i=1; i<arr.length; i++){  //found new unique
+            if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];       // place unique
+            }
+        }
+
+         int newLen = j + 1;
+
+        // Print output here
+        System.out.println("New length: " + newLen);
+        System.out.print("Array after removing duplicates: ");
+        for (int k = 0; k < newLen; k++) {
+            System.out.print(arr[k] + " ");
+        }
+        System.out.println(); // line break
+    }
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5 };
-        int k = 2;
-        int n = arr.length;
+        //int k = 2;
+        //int n = arr.length;
 
-        optRotation(arr, n, k);
+        // check if array is sorted
+
+        // decalre and intialize
+        boolean isAscending = true;
+        boolean isDescending = true;
+
+        // traverse - access every element
+        for (int i = 0; i < arr.length - 1; i++) {
+            // condition not desecnding
+            if (arr[i] < arr[i + 1]) {
+                isDescending = false;
+            } else if (arr[i] > arr[i + 1]) {
+                isAscending = false;
+            }
+        }
+
+        // condition - print ascending & decending
+        if (isAscending) {
+            System.out.println("Array is sorted in Ascending order");
+            removeDublicate(arr);
+        } else if (isDescending) {
+            System.out.println("Array is sorted in Descending order");
+            removeDublicate(arr);
+        } else {
+            System.out.println("Array is not sorted means you can't remove dublicate elements");
+        }
     }
 }
