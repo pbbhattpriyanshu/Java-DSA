bd2d71738e2496fe05e0a7657b182a80728f49a4
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-06-13 23:04:08 +0530

    Literals

---
+++ literals.java
@@ -0,0 +1,7 @@
+public class literals {
+    public static void main(String[] args) {
+        //binary numbers - 0b = binary and 1011 -> binary values 
+        int num = 0b1011;
+        System.out.println(num);
+    }
+}
