ab937962bf26ad4cb1804b87e5f7860575a60a0b
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-06-25 22:12:25 +0530

    Add parameterized constructor and encapsulation methods to Employee class; create Topics.txt for OOP concepts

--- OOPs/Test.java
+++ OOPs/Test.java
@@ -16,25 +16,40 @@ class Employee {
     private int Salary = 150000;
     private String Address = "Sector 62";
     private String Signature;
+    private String Degree;
+    private Double CGPA;
 
-    //Constructor
-    Employee(){
+    // Constructor
+    Employee() {
         System.out.println("This is the information of Employee");
     }
 
-    //Methods - values = set & get
+    public Employee(String deg, double grade) {
+        Degree = deg;
+        CGPA = grade;
+    }
+
+    // Methods - values = set & get
     public int getSalary() {
         return Salary;
     }
+
+    public String getDegree() {
+        return Degree;
+    }
+
     public String getAddress() {
         return Address;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setSignature(String sign) {
         Signature = sign;
     }
+
     public String getSignature() {
         return Signature;
     }
@@ -44,10 +59,12 @@ class Employee {
 class Test {
     public static void main(String[] args) {
         Employee obj = new Employee();
-        
+        Employee obj1 = new Employee("BCA", 7.1);
+
         obj.setName("Piyush");
         obj.setSignature("108PB");
         System.out.println(obj.name);
-        System.out.println(obj.getSignature());       
+        System.out.println(obj.getSignature());
+        System.out.println(obj1.getDegree());
     }
 }
