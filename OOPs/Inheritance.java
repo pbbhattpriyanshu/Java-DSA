5c2cccd1f3c92752f8ffbcd428082d2c9ff53e1c
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-06-30 12:02:17 +0530

    Refactor Inheritance classes to implement methods and update constructors; enhance functionality with show() and config() methods

--- OOPs/Inheritance.java
+++ OOPs/Inheritance.java
@@ -41,11 +41,20 @@ class advCalc extends Calc {
 class A {
     //Parameterized Constructor
     A(String name){
-        System.out.println("in A class there is " + name);
+        //System.out.println("in A class there is " + name);
     }
 
     A(){
-        System.out.println("in A class");
+        //System.out.println("in A class");
+    }
+
+    //Methods
+    public void show(){
+        System.out.println("in A show");
+    }
+
+    public void config(){
+        System.out.println("in A config");
     }
 }
 
@@ -53,19 +62,24 @@ class B extends A {
     //No-Argument Constructor
     B(){
         //super("Puish"); //super() - Argument
-        System.out.println("in B class");
+        //System.out.println("in B class");
     }
     B(int age){
         this(); //this class -> constructor call = "in B class"
-        System.out.println("my age is " + age);
+        //System.out.println("my age is " + age);
+    }
+
+    //Methods - method overriding - show().
+    public void show(){
+        System.out.println("in B show");
     }
 }
 
 class C extends B{
     //No-Argument Constructor = super() - write optional, default java automatically write 
     C(){
-        super(21);
-        System.out.println("in C class");
+        //super(21);
+        //System.out.println("in C class");
     }
 }
 public class Inheritance {
@@ -79,5 +93,8 @@ public class Inheritance {
         //System.out.println(ans1);
 
         C obj3 = new C();
+        B obj4 = new B();
+        obj4.show();
+        obj4.config();
     }
 }
